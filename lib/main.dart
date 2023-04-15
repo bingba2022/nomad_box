71974f99015102a117b90f0a5d019f0ef6f407a9
bingba2022 <bingba@gimbingbas-MacBook-Pro.local>
2023-04-15 14:51:37 +0900

    updated movie sections to display within its dates

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
+import 'package:transparent_image/transparent_image.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,9 +16,7 @@ const sectionColor = Color(0xFF9abdf5);
 const titleColor = Color(0xFFc0caf5);
 
 class MyApp extends StatelessWidget {
-  const MyApp({
-    super.key,
-  });
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -29,13 +28,11 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeScreen extends StatelessWidget {
-  HomeScreen({super.key});
+  HomeScreen({Key? key}) : super(key: key);
 
   final Future<List<MovieData>> popularMovies = ApiService.getMovies('popular');
-
   final Future<List<MovieData>> nowPlaying =
       ApiService.getMovies('now-playing');
-
   final Future<List<MovieData>> comingSoon =
       ApiService.getMovies('coming-soon');
 
@@ -78,6 +75,7 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             MovieSection(
+              key: const ValueKey('popular'),
               movieSection: popularMovies,
               sectionTitle: 'Popular Movies',
               boxHeight: 160.0,
@@ -86,6 +84,7 @@ class HomeScreen extends StatelessWidget {
               textSize: 16.0,
             ),
             MovieSection(
+              key: const ValueKey('now-playing'),
               movieSection: nowPlaying,
               sectionTitle: 'Now in Cinemas',
               boxHeight: 120.0,
@@ -94,6 +93,7 @@ class HomeScreen extends StatelessWidget {
               textSize: 13.0,
             ),
             MovieSection(
+              key: const ValueKey('coming-soon'),
               movieSection: comingSoon,
               sectionTitle: 'Coming Soon',
               boxHeight: 120.0,
@@ -109,19 +109,19 @@ class HomeScreen extends StatelessWidget {
 }
 
 class MovieSection extends StatelessWidget {
-  MovieSection({
-    super.key,
+  const MovieSection({
+    Key? key,
     required this.movieSection,
     required this.sectionTitle,
     required this.boxHeight,
     required this.boxWidth,
     required this.textWidth,
     required this.textSize,
-  });
+  }) : super(key: key);
 
   final Future<List<MovieData>> movieSection;
-  String sectionTitle;
-  double boxHeight, boxWidth, textWidth, textSize;
+  final String sectionTitle;
+  final double boxHeight, boxWidth, textWidth, textSize;
 
   @override
   Widget build(BuildContext context) {
@@ -143,20 +143,24 @@ class MovieSection extends StatelessWidget {
           ),
         ),
         SizedBox(
-          height: 200.0,
-          child: FutureBuilder(
-            future: movieSection,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Expanded(
-                  child: makeList(
-                      snapshot, boxHeight, boxWidth, textWidth, textSize),
-                );
-              }
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            },
+          height: 218.0,
+          child: Row(
+            children: [
+              FutureBuilder(
+                future: movieSection,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Expanded(
+                      child: makeList(
+                          snapshot, boxHeight, boxWidth, textWidth, textSize),
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ],
@@ -164,8 +168,11 @@ class MovieSection extends StatelessWidget {
   }
 }
 
-ListView makeList(AsyncSnapshot<List<MovieData>> snapshot, boxHeight, boxWidth,
-    textWidth, fontSize) {
+ListView makeList(AsyncSnapshot<List<MovieData>> snapshot, double? boxHeight,
+    double? boxWidth, double? textWidth, double? fontSize) {
+  if (!snapshot.hasData) {
+    return ListView();
+  }
   return ListView.separated(
     scrollDirection: Axis.horizontal,
     itemCount: snapshot.data!.length,
@@ -189,12 +196,19 @@ ListView makeList(AsyncSnapshot<List<MovieData>> snapshot, boxHeight, boxWidth,
               ],
             ),
             child: SizedBox(
-              height: boxHeight,
-              child: Image.network(
-                'https://image.tmdb.org/t/p/w500${movie.thumb}',
-                fit: BoxFit.cover,
-              ),
-            ),
+                height: boxHeight,
+                // if theres no backdrop image, replace it with poster image
+                child: movie.thumb != null
+                    ? FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: 'https://image.tmdb.org/t/p/w500${movie.thumb}',
+                        fit: BoxFit.cover,
+                      )
+                    : FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        image: 'https://image.tmdb.org/t/p/w500${movie.poster}',
+                        fit: BoxFit.cover,
+                      )),
           ),
           const SizedBox(
             height: 10,
@@ -223,49 +237,71 @@ ListView makeList(AsyncSnapshot<List<MovieData>> snapshot, boxHeight, boxWidth,
 
 class ApiService {
   static const baseUrl = 'https://movies-api.nomadcoders.workers.dev';
-  // static const popular = 'popular';
-  // static const now_playing = 'now-playing';
 
   static Future<List<MovieData>> getMovies(path) async {
     List<MovieData> movieInstances = [];
 
     final url = Uri.parse('$baseUrl/$path');
     final response = await http.get(url);
+
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
+
       final movies = data['results'];
 
       for (var movie in movies) {
-        final instance = MovieData.fromJson(movie);
-        movieInstances.add(instance);
+        final movieInstance = MovieData.fromJson(movie);
 
-        // sort by popularity
+        // getting released date of each movie
+        DateTime releasedDate = DateTime.parse(movieInstance.releaseDate);
+
         if (path == 'popular') {
+          movieInstances.add(movieInstance);
           movieInstances.sort(
             (a, b) => b.popularity.compareTo(a.popularity),
           );
-
-          // sort by release date
+        } else if (path == 'now-playing') {
+          DateTime maxDate = DateTime.parse(data['dates']['maximum']);
+          DateTime minDate = DateTime.parse(data['dates']['minimum']);
+          // min and max date boundaries including today
+          if ((releasedDate.isAtSameMomentAs(minDate)) ||
+              (releasedDate.isBefore(maxDate) &&
+                  releasedDate.isAfter(minDate))) {
+            movieInstances.add(movieInstance);
+            movieInstances.sort(
+              (a, b) => a.releaseDate.compareTo(b.releaseDate),
+            );
+          }
         } else if (path == 'coming-soon') {
-          movieInstances.sort(
-            (a, b) => a.releaseDate.compareTo(b.releaseDate),
-          );
+          DateTime maxDate = DateTime.parse(data['dates']['maximum']);
+          DateTime minDate = DateTime.parse(data['dates']['minimum']);
+
+          if ((releasedDate.isAtSameMomentAs(minDate)) ||
+              (releasedDate.isBefore(maxDate) &&
+                  releasedDate.isAfter(minDate))) {
+            movieInstances.add(movieInstance);
+            movieInstances
+                .sort((a, b) => a.releaseDate.compareTo(b.releaseDate));
+          }
         }
       }
+
       return movieInstances;
     }
-    throw Error();
+    throw Exception('failed to fetch movies: ${response.statusCode}');
   }
 }
 
 class MovieData {
-  final String title, thumb, id, releaseDate;
+  final String title, id, releaseDate;
+  final String? thumb, poster;
   double popularity;
 
   MovieData.fromJson(Map<String, dynamic> json)
       : title = json['title'],
-        thumb = json['backdrop_path'] ?? '', // null safety
+        thumb = json['backdrop_path'], // null safety
         id = json['id'].toString(),
         popularity = json['popularity'],
-        releaseDate = json['release_date'];
+        releaseDate = json['release_date'],
+        poster = json['poster_path'];
 }
